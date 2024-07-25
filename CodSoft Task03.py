3b07e68bec19b91a154c57105008bee2f37c7ef1
khushi972 <158293408+khushi972@users.noreply.github.com>
2024-07-25 16:47:51 +0530

    Calculator code

---
+++ CodSoft Task03.py
@@ -0,0 +1,28 @@
+#calculator
+
+first = input("enter first number:")
+
+operator = input("enter operator (+,-,*,/,%):")
+
+second= input("enter second number:")
+
+first = int(first)
+second = int(second)
+
+if operator == "+":
+    print(first + second)
+
+elif operator == "-":
+    print(first - second)
+
+elif  operator == "*":
+    print(first * second)
+
+elif operator == "/":
+    print(first / second)
+
+elif operator == "%":
+    print(first % second)        
+
+else:
+    print("worng operator")
